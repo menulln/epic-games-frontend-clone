8802cc9d4e7ceb1426bf27adc65e7abe1d6f4787
menulln <39300523+menulln@users.noreply.github.com>
2024-01-04 19:29:40 +0100

    Add searchComponent() and append to nav

--- src/navbar.js
+++ src/navbar.js
@@ -3,7 +3,35 @@ function navbarComponent(page) {
     const spacer = document.createElement('div');
 
     nav.appendChild(spacer);
-    
+    nav.appendChild(searchComponent());
+
+    function searchComponent() {
+        const div = document.createElement('div');
+        const arrowIcon = document.createElement('i');
+        const searchBar = document.createElement('input');
+        const discover = document.createElement('p');
+        const browse = document.createElement('p');
+        const news = document.createElement('p');
+
+        arrowIcon.classList.toggle('fa-solid');
+        arrowIcon.classList.toggle('fa-chevron-left');
+        searchBar.type = 'text';
+        discover.textContent = 'Discover';
+        browse.textContent = 'Browse';
+        news.textContent = 'News';
+        
+        div.appendChild(arrowIcon);
+        div.appendChild(searchBar);
+
+        if (page === 'store') {
+            div.appendChild(discover);
+            div.appendChild(browse);
+            div.appendChild(news);
+        }
+        
+        return div;
+    }
+
     return nav;
 }
 
