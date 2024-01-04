9b5535059fbe39a58dc9a48cd0aa9dfaf711b323
menulln <39300523+menulln@users.noreply.github.com>
2024-01-04 19:32:23 +0100

    Import navbarComponent and append to content div

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { sidebarComponent } from './sidebar';
+import { navbarComponent  } from './navbar';
 
 const content = document.querySelector('#content');
 
 content.appendChild(sidebarComponent());
+content.appendChild(navbarComponent('store'));
