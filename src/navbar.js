afa3ae38324fe19890415130350c4938dc16322e
menulln <39300523+menulln@users.noreply.github.com>
2024-01-04 19:29:05 +0100

    Create spacer div and append to nav

--- src/navbar.js
+++ src/navbar.js
@@ -1,6 +1,9 @@
 function navbarComponent(page) {
     const nav = document.createElement('nav');
+    const spacer = document.createElement('div');
 
+    nav.appendChild(spacer);
+    
     return nav;
 }
 
