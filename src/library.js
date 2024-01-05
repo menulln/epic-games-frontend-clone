850b0b43fe000889496eae737795c4a3c11776b0
menulln <39300523+menulln@users.noreply.github.com>
2024-01-05 14:27:33 +0100

    Add titleComponent() and append it to main

--- src/library.js
+++ src/library.js
@@ -1,5 +1,24 @@
 function libraryComponent() {
+    const main = document.createElement('main');
 
+    main.appendChild(titleComponent());
+
+    function titleComponent() {
+        const div = document.createElement('div');
+        const libraryTitle = document.createElement('h1');
+        const reloadIcon = document.createElement('i');
+
+        libraryTitle.textContent = 'Library';
+        reloadIcon.classList.toggle('fa-solid');
+        reloadIcon.classList.toggle('fa-rotate');
+
+        div.appendChild(libraryTitle);
+        div.appendChild(reloadIcon);
+
+        return div;
+    }
+
+    return main;
 }
 
 export { libraryComponent };
