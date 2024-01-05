659352e7eaaa341efe9f3f622160455da41cc9dc
menulln <39300523+menulln@users.noreply.github.com>
2024-01-05 14:31:48 +0100

    Add categoriesComponent() and append it to main

--- src/library.js
+++ src/library.js
@@ -2,6 +2,7 @@ function libraryComponent() {
     const main = document.createElement('main');
 
     main.appendChild(titleComponent());
+    main.appendChild(categoriesComponent());
 
     function titleComponent() {
         const div = document.createElement('div');
@@ -14,6 +15,23 @@ function libraryComponent() {
 
         div.appendChild(libraryTitle);
         div.appendChild(reloadIcon);
+
+        return div;
+    }
+
+    function categoriesComponent() {
+        const div = document.createElement('div');
+        const allParagraph = document.createElement('p');
+        const favoritesParagraph = document.createElement('p');
+        const plusParagraph = document.createElement('p');
+
+        allParagraph.textContent = 'All';
+        favoritesParagraph.textContent = 'Favorites';
+        plusParagraph.textContent = '+';
+
+        div.appendChild(allParagraph);
+        div.appendChild(favoritesParagraph);
+        div.appendChild(plusParagraph);
 
         return div;
     }
