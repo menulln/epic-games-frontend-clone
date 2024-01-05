7e34380323cda68cc9fc234819f98fc32f508c7b
menulln <39300523+menulln@users.noreply.github.com>
2024-01-05 21:30:32 +0100

    Import libraryComponent and append to content div

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { sidebarComponent } from './sidebar';
 import { navbarComponent  } from './navbar';
+import { libraryComponent } from './library';
 
 const content = document.querySelector('#content');
 
 content.appendChild(sidebarComponent());
 content.appendChild(navbarComponent('store'));
+content.appendChild(libraryComponent());
