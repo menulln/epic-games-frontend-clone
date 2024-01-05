f0dbb9a4ff8fd6b3d24af57b12f71ef3947dd168
menulln <39300523+menulln@users.noreply.github.com>
2024-01-05 21:15:35 +0100

    Add libraryItemsComponent() and append it to main

--- src/library.js
+++ src/library.js
@@ -4,8 +4,8 @@ function libraryComponent() {
     main.appendChild(titleComponent());
     main.appendChild(categoriesComponent());
     main.appendChild(sortViewComponent());
-    main.appendChild
-
+    main.appendChild(filtersTitleComponent());
+    main.appendChild(libraryItemsComponent());
 
     function titleComponent() {
         const div = document.createElement('div');
@@ -77,6 +77,69 @@ function libraryComponent() {
         filtersParagraph.textContent = 'Filters';
 
         return filtersParagraph;
+    }
+
+    function libraryItemsComponent() {
+        const div = document.createElement('div');
+
+        div.appendChild(createLibraryItem('ac', 'Assassin\'s Creed', true));
+        div.appendChild(createLibraryItem('aw', 'Alan Wake II', false));
+        div.appendChild(createLibraryItem('cp', 'Cyberpunk', true));
+        div.appendChild(createLibraryItem('crysis', 'Crysis 3', true));
+        div.appendChild(createLibraryItem('dead', 'Dead Island II', true));
+        div.appendChild(createLibraryItem('fn', 'Fortnite', true));
+        div.appendChild(createLibraryItem('fp', 'Frostpunk', false));
+        div.appendChild(createLibraryItem('genshin', 'Genshin Impact', true));
+        div.appendChild(createLibraryItem('gt', 'Galaxy Trek', false));
+        div.appendChild(createLibraryItem('gta', 'Grand Theft Auto', true));
+        div.appendChild(createLibraryItem('honkai', 'Honkai: Star Rail', false));
+        div.appendChild(createLibraryItem('kena', 'Kena: Bridge of Spirits', false));
+        div.appendChild(createLibraryItem('lotr', 'Lord of the Rings', false));
+        div.appendChild(createLibraryItem('rl', 'Rocket League', true));
+        div.appendChild(createLibraryItem('se', 'Sniper Elite 5', false));
+        div.appendChild(createLibraryItem('swbf', 'Star Wars: Battlefront II', true));
+        div.appendChild(createLibraryItem('swjs', 'Star Wars: Jedi Survivor', false));
+        div.appendChild(createLibraryItem('thps', 'Tony Hawk\'s Pro Skater', true));
+        div.appendChild(createLibraryItem('twau', 'The Wolf Amon Us II', false));
+        div.appendChild(createLibraryItem('valo', 'Valorant', true));
+
+        function createLibraryItem(imgName, title, isInstalled) {
+            const item = document.createElement('div');
+            const img = document.createElement('img');
+            const titleDiv = document.createElement('div');
+            const titleParagraph = document.createElement('p');
+            const ellipsisIcon = document.createElement('i');
+            const launchDiv = document.createElement('div');
+            const launchIcon = document.createElement('i');
+            const launchParagraph = document.createElement('p');
+
+            img.src = `../src/images/games/portrait_${imgName}.png`;
+            titleParagraph.textContent = title;
+            ellipsisIcon.classList.toggle('fa-solid');
+            ellipsisIcon.classList.toggle('fa-ellipsis');
+            launchIcon.classList.toggle('fa-solid');
+            
+            if (isInstalled) {
+                launchIcon.classList.toggle('fa-play');
+                launchParagraph.textContent = 'Launch';
+            } else {
+                launchIcon.classList.toggle('fa-download');
+                launchParagraph.textContent = 'Install';
+            }
+
+            titleDiv.appendChild(titleParagraph);
+            titleDiv.appendChild(ellipsisIcon);
+            launchDiv.appendChild(launchIcon);
+            launchDiv.appendChild(launchParagraph);
+
+            item.appendChild(img);
+            item.appendChild(titleDiv);
+            item.appendChild(launchDiv);
+
+            return item;
+        }
+
+        return div;
     }
 
     return main;
