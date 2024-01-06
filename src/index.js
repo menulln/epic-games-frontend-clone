d3ef6ce5009463948c1cbf4e146da00a471a3041
menulln <39300523+menulln@users.noreply.github.com>
2024-01-06 13:20:23 +0100

    Import ./layout.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { sidebarComponent } from './sidebar';
 import { navbarComponent  } from './navbar';
 import { libraryComponent } from './library';
 import './global.css';
+import './layout.css';
 
 const content = document.querySelector('#content');
 
