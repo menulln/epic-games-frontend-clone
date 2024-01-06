e07e56bf754a7cc121876b204fd23f7f45f8e7f5
menulln <39300523+menulln@users.noreply.github.com>
2024-01-06 13:06:27 +0100

    Import ./reset.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { sidebarComponent } from './sidebar';
 import { navbarComponent  } from './navbar';
 import { libraryComponent } from './library';
+import './reset.css';
 
 const content = document.querySelector('#content');
 
