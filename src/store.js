a933c733e2e514ab58b4cf209ced2dc96a6fac78
menulln <39300523+menulln@users.noreply.github.com>
2024-01-07 14:52:36 +0100

    Add and append saleComponent()

--- src/store.js
+++ src/store.js
@@ -1,5 +1,27 @@
 function storeComponent() {
+    const main = document.createElement('main');
 
+    main.appendChild(saleComponent());
+    
+    function saleComponent() {
+        const article = document.createElement('article');
+
+        div.appendChild(saleBannerComponent('festive-sale'));
+        div.appendChild(saleBannerComponent('voucher'));
+        div.appendChild(saleBannerComponent('gifts'));
+
+        function saleBannerComponent(type) {
+            const img = document.createElement('img');
+
+            img.src = `../src/images/store_banners/banner_${type}.png`;
+
+            return img;
+        }
+
+        return article;
+    }
+
+    return main;
 }
 
 export { storeComponent };
